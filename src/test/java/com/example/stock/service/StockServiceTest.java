18e4e17ab19c7839215c30bfc13d1004747feee4
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-17 22:28:43 +0900

    test: "Named Lock" - Named Lock 레파지토리,퍼사드 추가 - 서비스 로직에 propagation 옵션 설정 - 테스트 케이스 작성

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
+import com.example.stock.facade.NamedLockStockFacade;
 import com.example.stock.facade.OptimisticLockStockFacade;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -26,6 +27,9 @@ class StockServiceTest {
 
     @Autowired
     private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private NamedLockStockFacade namedLockStockFacade;
 
     @Autowired
     private StockRepository stockRepository;
@@ -148,9 +152,32 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        // Pessimisitic Lock
         assertEquals(0,stock.getQuantity());
     }
 
 
+
+    // Named Lock
+    @Test
+    public void 동시에_100개의_요청_V4() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    namedLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0,stock.getQuantity());
+    }
 }
