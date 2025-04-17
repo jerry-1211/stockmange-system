b623e90ca874d335d805dd4f966caebdd07fc053
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-17 17:29:51 +0900

    feat(stock): 도메인,리포지토리,서비스 로직 구현 - 리포지토리는 Spring Data JPA

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,31 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class StockService {
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity){
+        // Stock 조회
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        // 재고 감소 후
+        stock.decrease(quantity);
+
+        //갱신된 값 저장
+        stockRepository.save(stock);
+
+
+    }
+}
